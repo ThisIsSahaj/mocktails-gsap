8bfc9e7e892fec06cfbcdfc7a25a09dd68df1be5
thisissahaj <s21sahaj@gmail.com>
2025-07-04 22:27:36 +0530

    video scroll animation

--- README.md
+++ README.md
@@ -5,3 +5,15 @@
 - @gsap.react
 - react-responsive : will help with animation logic and layer behaviour based on screen size
 - tailwindcss @tailwindcss/vite
+
+## install
+- FFMPEG - https://ffmpeg.org/
+- in videos, we have frames only in a few seconds
+- but for scrub based animations on a video, we need frames very frequently, i.e. we need every single frame to be a keyframe
+- therefore we use FFMPEG to fix this issue
+
+- you just need to download it in your system & run the following command
+
+```
+ffmpeg -i input.mp4 -vf scale=960:-1 -movflags faststart -vcodec libx264 -crf 20 -g 1 -pix_fmt yuv420p output.mp4
+```
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,23 +1,34 @@
 import { useGSAP } from "@gsap/react"
 import gsap from "gsap"
 import { SplitText } from "gsap/all"
+import { useRef } from "react"
+import { useMediaQuery } from "react-responsive"
 
 const Hero = () => {
-    useGSAP(()=>{
+
+    // taking video referene
+    const videoRef = useRef();
+
+    // checking if device is mobile (for responsiveness of video)
+    const isMobile = useMediaQuery({ maxWidth: 767 })  //if its upto 767 then it'll be mobile
+
+
+
+    useGSAP(() => {
 
         // splitting title
-        const heroSplit = new SplitText('.title',{
+        const heroSplit = new SplitText('.title', {
             type: 'chars, words' //split in both characters and words
         })
-        
+
         // splitting paragraph
-        const paragraphSplit = new SplitText('.subtitle',{
+        const paragraphSplit = new SplitText('.subtitle', {
             type: 'lines' //split into only lines
         })
 
         // add gradient class to each char of title
         heroSplit.chars.forEach((char) => char.classList.add('text-gradient'));
-        
+
         // animate each char of title
         gsap.from(heroSplit.chars, {
             yPercent: 100,
@@ -28,26 +39,54 @@ const Hero = () => {
 
         // animate lines of paragraph
         gsap.from(paragraphSplit.lines, {
-            opacity:0,
-            yPercent:100,
+            opacity: 0,
+            yPercent: 100,
             duration: 1.8,
-            ease:'expo.out',
+            ease: 'expo.out',
             stagger: 0.05,
             delay: 1,
         });
 
         gsap.timeline({
-            scrollTrigger:{
-                trigger:"#hero", //trigger at hero section
+            scrollTrigger: {
+                trigger: "#hero", //trigger at hero section
                 start: 'top top', // when the top of the hero section hits the top of the screen
                 end: 'bottom top',
                 scrub: true
             }
         })
-        .to('.right-leaf', {y: 200}, 0)
-        .to('.left-leaf', {y: -200}, 0)
+            .to('.right-leaf', { y: 200 }, 0)
+            .to('.left-leaf', { y: -200 }, 0)
 
-    },[])
+
+        // VIDEO ANIMATION
+        const startValue = isMobile ? 'top 50%' : 'center 60%';   // when top of video reaches 50% down the screen, animation starts   //similarly when the center of the video reaches 60% down the screen, animation starts
+        const endValue = isMobile ? '120% top' : 'bottom top';    // when 120% of video has crossed the top of the screen, animation stops  //similarly, when the bottom of the video has reached the top of the screen, animation stops 
+
+        // VIDEO Animation TIMELINE
+        const videoTimeline = gsap.timeline({
+            scrollTrigger: {
+                trigger: 'video',
+                start: startValue,
+                end: endValue,
+                scrub: true,
+                pin: true,  // this will keep the video stuck on screen while scroll
+                // onEnter: () => videoRef.current.play(),
+                // onEnterBack: () => videoRef.current.play(),
+                // onLeave: () => videoRef.current.pause(),
+                // onLeaveBack: () => videoRef.current.pause(),
+            }
+        })
+        
+        videoRef.current.onloadedmetadata = () => {
+            videoTimeline.to(videoRef.current, {
+                currentTime: videoRef.current.duration   // update the current time based on video duration
+            })
+        }
+
+
+
+    }, [])
     return (
         <>
             <section id="hero" className="noisy">
@@ -86,6 +125,18 @@ const Hero = () => {
                 </div>
 
             </section>
+
+            {/* video section */}
+            <div className="video absolute inset-0 ">
+                <video
+                    ref={videoRef}
+                    src="/videos/output.mp4"
+                    muted
+                    playsInline  //to hide any video element like trackbar (videoPlayer)
+                    preload="auto"  //coz we want to load it automatically as the user opens the page
+                />
+
+            </div>
         </>
     )
 }
